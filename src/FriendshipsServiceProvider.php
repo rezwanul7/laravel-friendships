37a9f0b5bb52ffc52f35faf60ee98c2ed9324b44
Rezwanul Islam <rezwanul7@gmail.com>
2020-07-17 12:28:52 +0600

    removing loadMigrations

--- src/FriendshipsServiceProvider.php
+++ src/FriendshipsServiceProvider.php
@@ -16,7 +16,7 @@ class FriendshipsServiceProvider extends ServiceProvider
         if (class_exists('CreateFriendshipsTable') || class_exists('CreateFriendshipsGroupsTable')) {
             return;
         }
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+//        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
         $this->publishes([
             __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_table.stub.php'        => database_path('migrations') . '/' . date('Y_m_d_His', time()) . '_create_friendships_table.php',
             __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_groups_table.stub.php' => database_path('migrations') . '/' . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
